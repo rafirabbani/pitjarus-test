966d0ebbff2da403ed30443086cc81cdd998dc8a
Rafi <rafirabbani103@gmail.com>
2022-09-03 14:25:48 +0700

    api done

--- api/controller/ReportController.js
+++ api/controller/ReportController.js
@@ -1,14 +1,35 @@
 const ReportModel = require('../models/Report');
 
 const getProductReport = async (request, response) => {
-    const { productId } = request.query;
+    const { areaId, startDate, endDate } = request.query;
     try {
-        if (productId) {
-            const reportData = await ReportModel.getAllReport(productId);
-            return response.status(200).send(reportData);
+        if (areaId) {
+            if (startDate && endDate) {
+                const _startDate = new Date(startDate);
+                const _endDate = new Date(endDate);
+                if (_startDate > _endDate) {
+                    return response.status(400).send({status: 'error', message:'Invalid Date Range'});
+                }
+            }
+            const storeList = await ReportModel.getStoreList(areaId);
+            if(storeList.length < 1) {
+                return response.status(404).send({status: 'error', message: 'Store not Exist in Area'});
+            }
+            const sales = {};
+            for (const store of storeList) {
+                const storeSales = await ReportModel.getStoreReportByStoreId({storeId: store.store_id, startDate, endDate});
+                if (storeSales.length < 1) {
+                    return response.status(404).send({status: 'error', message: 'Data not found'});
+                }
+                storeSales.map(data => {
+                    data['percentage'] = Math.round(data.total_sales / data.total_product * 100);
+                })
+                sales[store.area_name] = storeSales;
+            }
+            return response.status(200).send(sales);
         }
         else {
-            response.status(400).send({status: 'error', message: 'Product Id is required'});
+            response.status(400).send({status: 'error', message: 'Area Id is required'});
         }
     }
     catch (err) {
--- api/models/Report.js
+++ api/models/Report.js
@@ -6,6 +6,32 @@ const getAllReport = (productId) => {
     return DB.sqlPromise(query, params);
 };
 
+const getStoreList = (areaId) => {
+    const query = 'SELECT * FROM store_area AS A LEFT JOIN store AS S ON A.area_id = S.area_id WHERE A.area_id IN ?';
+    const params = [areaId];
+    return DB.sqlPromise(query, params);
+};
+
+const getStoreReportByStoreId = (payload) => {
+    const { storeId, startDate, endDate } = payload;
+    const params = [storeId];
+    let query = 'SELECT *, COUNT(CASE WHEN R.compliance = 1 THEN 1 END) AS total_sales, COUNT(R.product_id) AS total_product FROM report_product AS R LEFT JOIN product AS P ON R.product_id = P.product_id LEFT JOIN product_brand AS B ON P.brand_id = B.brand_id WHERE store_id = ?';
+    if (startDate) {
+        query += ' AND tanggal >= ?';
+        params.push(startDate);
+    }
+    if (endDate) {
+        query += ' AND tanggal <= ?';
+        params.push(endDate);
+    }
+
+    query += ' GROUP BY brand_name'
+    
+    return DB.sqlPromise(query, params);
+}
+
 module.exports = {
-    getAllReport
+    getAllReport,
+    getStoreList,
+    getStoreReportByStoreId
 };
